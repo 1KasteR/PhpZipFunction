3cb018f7bc51d10e89bd0809e86cce8ce4904854
1KasteR <kaster@yahoo.com>
2013-10-31 00:33:32 -0700

    Update function.php

--- function.php
+++ function.php
@@ -1,22 +1,24 @@
-<?php $path=BASEDIR."tmp";
-$zip = new ZipArchive;
-$zip->open('file.zip', ZipArchive::CREATE);
-if (false !== ($dir = opendir($path)))
-     {
-         while (false !== ($file = readdir($dir)))
-         {
-             if ($file != '.' && $file != '..')
-             {
-                       $zip->addFile($path.DIRECTORY_SEPARATOR.$file);
-                       //delete if need
-                       if($file!=='important.txt') 
-                         unlink($path.DIRECTORY_SEPARATOR.$file);
-             }
-         }
-     }
-     else
-     {
-         die('Can\'t read dir');
-     }
+<?php
+function ZipDir($dir, $filename)
+{
+// increase script timeout value
+ini_set("max_execution_time", 300);
+// create object
+$zip = new ZipArchive();
+// open archive
+if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
+die ("Could not open archive");
+}
+// initialize an iterator
+// pass it the directory to be processed
+$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+// iterate over the directory
+// add each file found to the archive
+foreach ($iterator as $key=>$value) {
+$zip->addFile(realpath($key), $key) or die ("ERROR: Could not add file: $key");
+}
+// close and save archive
 $zip->close();
+echo "Archive created successfully.";
+}
 ?>
